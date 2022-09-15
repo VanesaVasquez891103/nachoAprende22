f80bbc13871d152114da0de83916ab2bd88a8b23
Usuario <Usuario@B13-205-22873>
2022-09-15 11:29:16 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using nachoAprende;
+
+namespace nachoAprende
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite los numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2= int.Parse(Console.ReadLine());
+
+            int resultado = (nachoAprende.clslib.suma(num1,num2));
+
+            Console.WriteLine("El resultado es: "+resultado);
+         
+
+         
+        }
+    }
+}
---
+++ clslib.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nachoAprende
+{
+    class clslib
+    {
+        public static int suma(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+        public static int resta(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+        public static int rmultiplicacion(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+    }
+}
